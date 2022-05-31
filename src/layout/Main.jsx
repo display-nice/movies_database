e50daf398b0c9a608e0590331da7aa3c843dc758
Oleg Grigorev <osrolex@yandex.ru>
2022-05-31 14:25:58 +0500

    small api key rework (env.local)

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -3,20 +3,22 @@ import {Movies} from '../components/Movies';
 import {Search} from '../components/Search';
 import {Preloader} from '../components/Preloader';
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+
 class Main extends React.Component {
     state = {
         movies: [],
         loading: true
     }
     componentDidMount() {
-        fetch('http://www.omdbapi.com/?apikey=7e2ac28f&s=matrix')
+        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
             .then(response => response.json())
             .then(jsonContent => this.setState({movies: jsonContent.Search, loading: false}))
     }
 
     searchMovies = (searchText, type = 'all') => {
         this.setState({loading: true});
-        fetch(`http://www.omdbapi.com/?apikey=7e2ac28f&s=${searchText}${type !== 'all' ? `&type=${type}` : ''}`)
+        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${searchText}${type !== 'all' ? `&type=${type}` : ''}`)
             .then(response => response.json())
             .then(jsonContent => this.setState({movies: jsonContent.Search, loading: false}))
     }
@@ -26,7 +28,9 @@ class Main extends React.Component {
         return <main className="container content">
             <Search searchMovies={this.searchMovies}/>            
             {/* разобраться, почему здесь не работает иф и как правильно его использовать */}
-            { loading ? (<Preloader/>) : (<Movies movies={movies}/>) } 
+            
+            { loading ? (<Preloader/>) : (<Movies movies={movies}/>) }
+
         </main>
     }
 }
